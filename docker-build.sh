64cf2599d15ad3303cddcf0a416a1c8bdd94d749
Liu Yue <hangxingliu@gmail.com>
2021-12-30 17:30:55 +0800

    update sccripts

---
+++ docker-build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+pushd "$( dirname "${BASH_SOURCE[0]}" )/.." || exit 1;
+
+set -x;
+docker run -it --rm --entrypoint /bin/sh -v "$(pwd):/workspace" -p 18080:8080 node:14 -c "cd /workspace && npm run build"

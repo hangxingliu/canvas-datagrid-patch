444460597befae64ab7fcad681bce52b645e7eac
Liu Yue <hangxingliu@gmail.com>
2022-01-19 00:32:34 +0800

    update for container

--- docker-build.sh
+++ docker-build.sh
@@ -3,4 +3,4 @@
 pushd "$( dirname "${BASH_SOURCE[0]}" )/.." || exit 1;
 
 set -x;
-docker run -it --rm --entrypoint /bin/sh -v "$(pwd):/workspace" -p 18080:8080 node:14 -c "cd /workspace && npm run build"
+docker run -it --rm --entrypoint /bin/sh -v "$(pwd):/workspace" -p 18080:8080 node:14x -c "cd /workspace && npm run build"
--- docker-start.sh
+++ docker-start.sh
@@ -3,4 +3,4 @@
 pushd "$( dirname "${BASH_SOURCE[0]}" )/.." || exit 1;
 
 set -x;
-docker run -it --rm --entrypoint /bin/sh -v "$(pwd):/workspace" -p 18080:8080 node:14 -c "cd /workspace && ./node_modules/.bin/webpack serve --host 0.0.0.0"
+docker run -it --rm --entrypoint /bin/sh -v "$(pwd):/workspace" -p 18080:8080 node:14x -c "cd /workspace && ./node_modules/.bin/webpack serve --host 0.0.0.0"

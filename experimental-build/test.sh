df3ff689aba461bbc4268d3810fa59420fbffbac
Liu Yue <hangxingliu@gmail.com>
2022-03-26 17:27:40 +0800

    fix test.sh

--- experimental-build/test.sh
+++ experimental-build/test.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-pushd "$( dirname "${BASH_SOURCE[0]}" )" || exit 1;
+goto_dir() {
+  pushd "$1" >/dev/null || exit 1;
+  echo "pwd: $(pwd)";
+}
+goto_dir "$( dirname "${BASH_SOURCE[0]}" )";
+
+swc="$(pwd)/node_modules/.bin/swc";
+node_swcrc="$(pwd)/node.swcrc"
 
 yarn run build || exit 1;
-if test -z PROJECT_ROOT; then
-  DEFAULT_PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../.. && pwd )";
+if test -z "$PROJECT_ROOT"; then
+  DEFAULT_PROJECT_ROOT="$( cd ../.. && pwd )";
   PROJECT_ROOT="${DEFAULT_PROJECT_ROOT}"
 fi
 
-pushd "$PROJECT_ROOT" || exit 1;
+goto_dir "$PROJECT_ROOT";
+
+swc_args=( lib --out-dir dist/node --config-file "${node_swcrc}" )
+echo "${swc} ${swc_args[@]} ...";
+"${swc}" "${swc_args[@]}" || exit 1;
+
 yarn run test:watch;
